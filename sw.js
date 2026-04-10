09d37ff179e165da1d90b6e985eb5c4e54d5f4af
davidfooter06-beep <davidfooter06@gmail.com>
2026-04-10 15:55:25 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,21 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Installed');
-});
-self.addEventListener('fetch', (e) => {});
+{
+  "name": "Ethio-Online-Job Pro",
+  "short_name": "EthioJob",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#1dbf73",
+  "description": "Freelancing Marketplace in Ethiopia",
+  "icons": [
+    {
+      "src": "assets/app-icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "assets/app-icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
